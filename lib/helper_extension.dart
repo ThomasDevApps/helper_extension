a8b58191f204f229c7daa6a16bffa759801b522b
Thomas <thomas.devapps@gmail.com>
2023-11-17 15:34:39 +0100

    Update helper_extension.dart

--- lib/helper_extension.dart
+++ lib/helper_extension.dart
@@ -1,4 +1,6 @@
 library helper_extension;
 
 export 'src/helper_extension_list.dart';
+export 'src/helper_extension_map.dart';
+export 'src/helper_extension_string.dart';
 export 'src/helper_extension_text.dart';
